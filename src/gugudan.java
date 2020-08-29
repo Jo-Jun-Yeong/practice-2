455d487fdd2b8589538ecbc557103270e07821bf
apple <apple@localhost>
2020-08-29 20:50:54 +0900

    initial commit

--- src/gugudan.java
+++ src/gugudan.java
@@ -1,61 +1,20 @@
 import java.util.Scanner;
 public class gugudan {
 	public static void main(String[] args) {
-		// 2단	
-//		System.out.println( "2");
-//		System.out.println(2 * 1);
-//		System.out.println(2 * 2);
-//		System.out.println(2 * 3);
-//		System.out.println(2 * 4);
-//		System.out.println(2 * 5);
-//		System.out.println(2 * 6);	
-//		System.out.println(2 * 7);
-		
-		// 3단 
-//		System.out.println( "3단");
-//		System.out.println(3 * 1);
-//		System.out.println(3 * 2);
-//		System.out.println(3 * 3);
-//		System.out.println(3 * 4);
-//		System.out.println(3 * 5);
-//		System.out.println(3 * 6);
-
-		
-//		 //4단
-//		System.out.println("4단");
-//		int result = 4 * 1;
-//		// **int 는 숫자값만 저장 가** (데이터 타입)
-//		System.out.println(result);
-//		result = 4 * 2;
-//		System.out.println(result);
-//		result = 4 * 3;
-//		System.out.println(result);
-//		result = 4 * 4;
-//		System.out.println(result);
-//		result = 4 * 5;
-//		System.out.println(result);
-//		result = 4 * 6;
-//		System.out.println(result);
-//		result = 4 * 7;
-//		System.out.println(result);
-//		result = 4 * 8;
-//		System.out.println(result);
-//		result = 4 * 9;
-//		System.out.println(result);
-//		
-		
-		System.out.println("구구단 중 출력할 단은>? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
-		System.out.println(number * 10);
+		System.out.println("입력한 숫자의 곱셉이 출력 :     ");
+	Scanner scanner = new Scanner(System.in);
+	int fx = scanner.nextInt();
+	if ( fx > 10) {
+		System.out.println("2~9사이의 숫자를 입력하세요");
 	}
-}
+	else if (fx < 2) {
+		System.out.println("2~9사이의 숫자를 력하세요");
+	}
+	else 
+		for (int i = 1; i < 10; i++) {
+			System.out.print("사용가 입력한 숫자      ");
+			System.out.println(fx + "*" + i + "=" +i*fx);
+		}
+	}
+	}
+	
